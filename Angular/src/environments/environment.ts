695b1efebef49a1a2a6fcb9538793f7e27b84430
Niphy-10637063 <128737044+Niphy-10637063@users.noreply.github.com>
2023-08-01 21:55:33 +0100

    Deployement variables added

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment: any = {
-    // baseUrl: 'https://my-kitchen-flask-application.azurewebsites.net'
-    baseUrl:'https://2663-2a02-8084-4140-1700-d468-6f84-c55-2820.ngrok-free.app'
+    baseUrl:'https://my-kitchen-web-application-backend-latest.onrender.com'
   };
   
